c0e7b5d2aeb0edc89b776ad6bec6e1cdf29a398c
kahi <waifumaximalist@gmail.com>
2026-02-24 05:22:18 +0800

    fix custom bash

--- .homelab/bash/custom.bash
+++ .homelab/bash/custom.bash
@@ -3,5 +3,5 @@ export PATH="$HOME/.homelab/bin:$PATH"
 export EDITOR='vim'
 export VISUAL="$EDITOR"
 
-command -v tty-color-tool &>/dev/null && eval "$(tty-color-tool rc-bash)"
 command -v tty-color-tool &>/dev/null && eval "$(tty-color-tool set-soft $HOME/.homelab/tty-colors/tty-colors-no-lg.txt)"
+command -v tty-color-tool &>/dev/null && eval "$(tty-color-tool rc-bash)"
